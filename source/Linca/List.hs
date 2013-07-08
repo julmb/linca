722feb6ea01c09bedea7515a35ed4036d96f3991
Julian Brunner <julianbrunner@gmail.com>
2013-07-08 18:53:26 +0200

    fixed a bug and added error handling in Linca.List.replaceAt

--- source/Linca/List.hs
+++ source/Linca/List.hs
@@ -3,8 +3,10 @@ module Linca.List where
 import Data.List
 
 replaceAt :: Integer -> a -> [a] -> [a]
+replaceAt index item list | index < 0                      = error "Linca.List.replaceAt: negative index"
+replaceAt index item list | index > genericLength list - 1 = error "Linca.List.replaceAt: index too large"
 replaceAt index item list =
 	let
-		head = genericTake (index - 1) list
-		tail = genericDrop index list
+		head = genericTake index list
+		tail = genericDrop (index + 1) list
 	in head ++ [item] ++ tail
