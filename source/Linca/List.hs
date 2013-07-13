a8359f2b8986feb9026cbfe00bce6af331a7b184
Julian Brunner <julianbrunner@gmail.com>
2013-07-13 18:12:26 +0200

    added some more functions to List module

--- source/Linca/List.hs
+++ source/Linca/List.hs
@@ -10,3 +10,12 @@ replaceAt index item list =
 		head = genericTake index list
 		tail = genericDrop (index + 1) list
 	in head ++ [item] ++ tail
+
+rotateLeft :: [a] -> [a]
+rotateLeft list = tail list ++ [head list]
+
+rotateRight :: [a] -> [a]
+rotateRight list = [last list] ++ init list
+
+indices :: Integer -> [Integer]
+indices count = [0 .. (count - 1)]
