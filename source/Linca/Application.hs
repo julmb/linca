0eb2e850861727adfc5e0d3b2f8a033be0cbb912
Julian Brunner <julianbrunner@gmail.com>
2013-07-15 13:11:58 +0200

    added Application, improved Lens

---
+++ source/Linca/Application.hs
@@ -0,0 +1,32 @@
+module Linca.Application where
+
+import Control.Monad.State
+import Linca.Timing
+
+data Application state display = Application
+	{
+		frameDuration :: Double,
+		initialState :: state,
+		updateState :: Double -> state -> state,
+		getDisplay :: state -> display
+	}
+
+loopApplication :: Application state display -> (display -> IO ()) -> Double -> StateT state IO ()
+loopApplication application present lastTime = do
+	currentTime <- lift getTime
+	let passedDuration = currentTime - lastTime
+	let waitDuration = frameDuration application - passedDuration
+	lift (delay waitDuration)
+	currentTime <- lift getTime
+
+	modify (updateState application (currentTime - lastTime))
+	display <- gets (getDisplay application)
+	lift (present display)
+
+	loopApplication application present currentTime
+
+runApplication :: Application state display -> (display -> IO ()) -> IO ()
+runApplication application present = do
+	initialTime <- getTime
+	execStateT (loopApplication application present initialTime) (initialState application)
+	return ()
--- source/Linca/Lens.hs
+++ source/Linca/Lens.hs
@@ -3,10 +3,14 @@ module Linca.Lens where
 import Control.Lens
 import Control.Monad.State
 
-infix 4 ..=
+infix 4 ...=
 
-(..=) :: MonadState s m => ASetter' s s' -> State s' a -> m ()
-property ..= updateProperty = property %= execState updateProperty
+(...=) :: MonadState s m => Lens' s s' -> State s' a -> m a
+property ...= update = do
+	oldState <- use property
+	let (value, newState) = runState update oldState
+	property .= newState
+	return value
 
 genericIx :: (Integral i, Num (Index m), Ixed f m) => i -> IndexedLensLike' (Index m) f m (IxValue m)
 genericIx = ix . fromIntegral
