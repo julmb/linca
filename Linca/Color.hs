47c52d05ad7848d434cac8c4eaf7ad928b67ea77
Julian Brunner <julianbrunner@gmail.com>
2015-11-12 04:12:45 +0100

    removed Color.Binary

--- Linca/Color.hs
+++ Linca/Color.hs
@@ -1,11 +1,9 @@
 module Linca.Color
 (
 	module Linca.Color.Color,
-	module Linca.Color.Alpha,
-	module Linca.Color.Binary
+	module Linca.Color.Alpha
 )
 where
 
 import Linca.Color.Color
 import Linca.Color.Alpha
-import Linca.Color.Binary
