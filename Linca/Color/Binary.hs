bd31821b9f15a3fd0470a5f89538c76547f609f7
Julian Brunner <julianbrunner@gmail.com>
2015-11-12 03:18:31 +0100

    fixed binary color conversion

--- Linca/Color/Binary.hs
+++ Linca/Color/Binary.hs
@@ -4,9 +4,10 @@ import Data.Word
 import Linca.Color.Color
 
 fromRGB24 :: (Word8, Word8, Word8) -> Color
-fromRGB24 (red, green, blue) = rgb (fromByte red) (fromByte green) (fromByte blue)
-	where fromByte byte = fromIntegral byte / 0xFF
+fromRGB24 (red, green, blue) = rgb (fromByte red) (fromByte green) (fromByte blue) where
+	fromByte byte = fromIntegral byte / 0xFF
 
 toRGB24 :: Color -> (Word8, Word8, Word8)
-toRGB24 color = (toByte (red color), toByte (green color), toByte (blue color))
-	where toByte value = round (value * 0xFF)
+toRGB24 color = (toByte (red color), toByte (green color), toByte (blue color)) where
+	toByte 1 = 0xFF
+	toByte value = truncate (value * 0x100)
