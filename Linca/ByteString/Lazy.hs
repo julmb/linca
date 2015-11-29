316557b4695772775e477695d82916011ad44172
Julian Brunner <julianbrunner@gmail.com>
2015-11-29 21:24:44 +0100

    renamed bytestring module

--- Linca/ByteString/Lazy.hs
+++ Linca/ByteString/Lazy.hs
@@ -1,11 +1,9 @@
-module Linca.ByteString (fold, replace) where
+module Linca.ByteString.Lazy (fold, replace) where
 
 import Numeric.Natural
 import Data.Monoid
 import Data.Word
 import qualified Data.ByteString.Lazy as BL
-
--- TODO: separate BL and BS for consistent prefixes
 
 fold :: (Word8 -> a -> a) -> BL.ByteString -> a -> a
 fold = flip . BL.foldl . flip
