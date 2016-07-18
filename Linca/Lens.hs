04a484c088d7a717e59e19bfc94bab1a10efa8b3
Julian Brunner <julianbrunner@gmail.com>
2016-07-18 21:28:34 +0200

    refactored lens library

--- Linca/Lens.hs
+++ Linca/Lens.hs
@@ -1,30 +1,24 @@
-{-# LANGUAGE RankNTypes #-}
+module Linca.Lens (Lens, lens, get, set, over, at) where
 
-module Linca.Lens () where
-
--- TODO: change this to import Data.Functor.Const after upgrading to base 4.9
-import Control.Applicative
+import Data.Map
+import Data.Functor.Const
 import Data.Functor.Identity
 
 type Lens object value = forall f. Functor f => (value -> f value) -> (object -> f object)
-type View object value = object -> value
-type Over object value = (value -> value) -> (object -> object)
-type Set object value = value -> (object -> object)
 
-lens :: View object value -> Over object value -> Lens object value
--- view :: object -> value
--- over :: (value -> value) -> (object -> object)
--- f :: value -> f value
--- object :: object
--- fmap :: (a -> b) -> (f a -> f b)
--- result :: f object
-lens view over f object = fmap (\value -> over (const value) object) (f (view object))
+lens :: (object -> value) -> (value -> object -> object) -> Lens object value
+lens get set f object = fmap (flip set object) (f (get object))
 
-lens' :: View object value -> Set object value -> Lens object value
-lens' view set f object = fmap (\value -> set value object) (f (view object))
+get :: Lens object value -> (object -> value)
+get lens = getConst . lens Const
 
-view :: Lens object value -> View object value
-view lens = getConst . lens Const
+set :: Lens object value -> value -> (object -> object)
+set lens value = runIdentity . lens (Identity . const value)
 
-over :: Lens object value -> Over object value
+over :: Lens object value -> (value -> value) -> (object -> object)
 over lens f = runIdentity . lens (Identity . f)
+
+at :: Ord key => key -> Lens (Map key value) value
+at key = lens g s where
+	g map = map ! key
+	s value map = insert key value map
