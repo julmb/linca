31149b528753a8328c91200ac5c909beddaef0bd
Julian Brunner <julianbrunner@gmail.com>
2016-07-22 07:39:39 +0200

    removed obsolete Cont functions

--- Linca/Monad.hs
+++ Linca/Monad.hs
@@ -1,12 +1,6 @@
-module Linca.Monad (foldMonad, runT, run) where
+module Linca.Monad (foldMonad) where
 
-import Control.Monad.Cont
+import Control.Monad
 
 foldMonad :: (Foldable t, Monad m) => (x -> a -> m a) -> (t x -> a -> m a)
 foldMonad = flip . foldM . flip
-
-runT :: Monad m => ContT r m r -> m r
-runT c = runContT c return
-
-run :: Cont r r -> r
-run c = runCont c id
